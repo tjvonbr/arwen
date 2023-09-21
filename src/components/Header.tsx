2e13476aad313a3c95c29f04750581fd88e0afea
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-09-21 00:00:08 -0700

    logo

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import Logo from "./Logo";
+
+export default function Header() {
+  return (
+    <header className="w-full px-10 py-5">
+      <Logo />
+    </header>
+  );
+}
---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Logo() {
+  return (
+    <Link
+      href="/"
+      className="text-2xl text-white font-black uppercase tracking-wide"
+    >
+      Arwen
+    </Link>
+  );
+}
